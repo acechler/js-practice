1223b76bfabada6991c11100ab9722a8f6cf4d7a
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-17 19:27:20 -0400

    implement feature

--- dom-fun/index.js
+++ dom-fun/index.js
@@ -1,1 +1,7 @@
-console.log('helloworld');
+
+const box = document.createElement('div');
+box.className = 'box';
+box.style.width = '64px';
+box.style.height = '64px';
+box.style.backgroundColor = `blue`;
+document.appendChild(box);
